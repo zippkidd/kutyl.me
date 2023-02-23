a8ac2a9fcec197981d725c19a641d487d19bc670
zippkidd <thomastylek@gmail.com>
2023-02-22 21:35:26 -0500

    standard --fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,26 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`
 })
 module.exports = {
   siteMetadata: {
-    title: `kutyl.me`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: 'kutyl.me',
+    siteUrl: 'https://www.yourdomain.tld'
   },
   plugins: [{
     resolve: 'gatsby-source-contentful',
     options: {
-      "accessToken": process.env.CONTENTFUL_ACCESS_TOKEN,
-      "spaceId": process.env.CONTENTFUL_SPACE_ID
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      spaceId: process.env.CONTENTFUL_SPACE_ID
     }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass", "gatsby-plugin-sitemap", {
+  }, 'gatsby-plugin-image', 'gatsby-plugin-sharp', 'gatsby-transformer-sharp', 'gatsby-plugin-sass', 'gatsby-plugin-sitemap', {
     resolve: 'gatsby-source-filesystem',
     options: {
-      "name": "images",
-      "path": "./src/images/"
+      name: 'images',
+      path: './src/images/'
     },
-    __key: "images"
+    __key: 'images'
   }]
-};
+}
