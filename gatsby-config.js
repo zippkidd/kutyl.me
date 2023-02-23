bcdf0d7f6f9ea2e2ba2c23e1de998ef3f69e0bf6
zippkidd <thomastylek@gmail.com>
2023-02-23 00:47:46 -0500

    U+1F9F9

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,18 +9,26 @@ module.exports = {
     title: 'kutyl.me',
     siteUrl: 'https://www.yourdomain.tld'
   },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      spaceId: process.env.CONTENTFUL_SPACE_ID
-    }
-  }, 'gatsby-plugin-image', 'gatsby-plugin-sharp', 'gatsby-transformer-sharp', 'gatsby-plugin-sass', 'gatsby-plugin-sitemap', {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      name: 'images',
-      path: './src/images/'
+  plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID
+      }
     },
-    __key: 'images'
-  }]
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sass',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/'
+      },
+      __key: 'images'
+    }
+  ]
 }
