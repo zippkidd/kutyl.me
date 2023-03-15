b961dd5c46f67a66c09b1b1e02ce0b52a7ab6f0f
zippkidd <thomastylek@gmail.com>
2023-03-15 00:46:29 -0400

    :construction: render RTE content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import useGetProjects from '../hooks/useGetProjects'
+import { renderRichText } from 'gatsby-source-contentful/rich-text'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import { Layout } from '../components/templates'
 import { Heading } from '../components/atoms/'
@@ -7,8 +8,8 @@ import { Heading } from '../components/atoms/'
 const IndexPage = () => {
   const projects = useGetProjects()
   const cardImage = getImage(projects[0].card)
-  console.log(projects[0].slug)
-  // console.log(projects)
+  // console.log(projects[0].slug)
+  // console.log(projects[0])
   // console.log(cardImage)
   return (
     <Layout>
@@ -19,6 +20,7 @@ const IndexPage = () => {
         {projects[0].title}
       </Heading>
       <GatsbyImage image={cardImage} alt='test image' />
+      {renderRichText(projects[0].content)}
     </Layout>
   )
 }
