c2aa3cfc2dfd4cc3ed32db63235aced9160bd0ba
zippkidd <thomastylek@gmail.com>
2023-03-28 22:02:43 -0400

    :twisted_rightwards_arrows: Merge branch feature/connect-to-contentful into branch master

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,29 @@
 import * as React from 'react'
+import useGetHomepage from '../hooks/useGetHomepage'
+import { renderRichText } from 'gatsby-source-contentful/rich-text'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import { Layout } from '../components/templates'
 import { Heading } from '../components/atoms/'
 
 const IndexPage = () => {
+  const data = useGetHomepage()
+  const banner = getImage(data.banner)
+  const { title, subtitle, content } = data
   return (
     <Layout>
       <Heading
         level='1'
-        size='3'
         id='testID'
       >
-        Cuddle Me
+        {title}
       </Heading>
-      Hello, World!
+      <Heading
+        level='2'
+      >
+        {subtitle}
+      </Heading>
+      <GatsbyImage image={banner} alt='test image' />
+      {renderRichText(content)}
     </Layout>
   )
 }
